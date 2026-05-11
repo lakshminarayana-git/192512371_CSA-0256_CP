9eb0b80b6f3bd17cfb8878bcec997f30895c4c2d
lakshminarayana-git <192512371.simats@saveetha.com>
2026-05-11 10:11:38 +0530

    Create PASS\FAIL.C

---
+++ OPERATORS/PASS\FAIL.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+ int marks;
+ printf("Enter your marks: ");
+ scanf("%d", &marks);
+
+ if (marks >= 50) 
+{
+printf("You passed!\n");
+}
+else 
+{
+ printf("You failed.\n");
+ }
+
+ return 0;
+
+}
